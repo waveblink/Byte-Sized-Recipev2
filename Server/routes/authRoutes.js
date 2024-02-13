de5c73f42ae570edcbe17ea35ad3542f0b186533
waveblink <maxwellhahn@gmail.com>
2024-02-13 13:39:07 -0600

    Small update to authRotes.js

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -10,6 +10,26 @@ const router = express.Router();
 const pool = new Pool({ connectionString: process.env.DATABASE_URL});
 
 router.post('/register', async (req, res) => {
+    const pool = {firstName, lastName, email, password} = req.body;
+
+    try {
+        
+        const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (userExists.rows.length > 0) {
+            return res.status(400).json({ message: "User already exists with that email." });
+
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        
+            const newUser = await pool.query("INSERT INTO users (firstName, lastName, email, password) VALUES ($1, $2, $3, $4 RETURNING *",
+            [firstName, lastName, email, hashedPassword]
+            );
+            res.status(201).json({user: newUser.rows[0]});
+    } catch (error) {
+        console.error(err);
+        res.status(500).json({ message: 'Error registering user' });
+    }
 
 });
 
